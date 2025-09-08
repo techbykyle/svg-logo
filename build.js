425feddcc3509b4ab6dc10a69e51c8c29b1f77ad
Kyle Melik <kyle@techbykyle.com>
2025-09-08 09:39:03 -0700

    Added three new logos, github, gitea and gitlab.

--- build.js
+++ build.js
@@ -6,24 +6,28 @@ const srcDir = path.join(__dirname, 'src')
 const distDir = path.join(__dirname, 'dist')
 
 function transpileFile(srcPath, distPath) {
-  const code = fs.readFileSync(srcPath, 'utf8')
-  const result = babel.transformSync(code, {
-    presets: ["@babel/preset-env", "@babel/preset-react"]
-  })
-  fs.mkdirSync(path.dirname(distPath), { recursive: true })
-  fs.writeFileSync(distPath, result.code)
+
+    const code = fs.readFileSync(srcPath, 'utf8')
+    const result = babel.transformSync(code, {
+        presets: ["@babel/preset-env", "@babel/preset-react"]
+    })
+
+    fs.mkdirSync(path.dirname(distPath), { recursive: true })
+    fs.writeFileSync(distPath, result.code)
 }
 
 function transpileDir(src, dist) {
-  fs.readdirSync(src).forEach(file => {
-    const srcPath = path.join(src, file)
-    const distPath = path.join(dist, file.replace(/\.jsx$/, '.js'))
-    if (fs.statSync(srcPath).isDirectory()) {
-      transpileDir(srcPath, path.join(dist, file))
-    } else if (file.endsWith('.jsx')) {
-      transpileFile(srcPath, distPath)
-    }
-  })
+    fs.readdirSync(src).forEach(file => {
+
+        const srcPath = path.join(src, file)
+        const distPath = path.join(dist, file.replace(/\.jsx$/, '.js'))
+
+        if (fs.statSync(srcPath).isDirectory()) {
+            transpileDir(srcPath, path.join(dist, file))
+        } else if (file.endsWith('.jsx')) {
+            transpileFile(srcPath, distPath)
+        }
+    })
 }
 
 fs.rmSync(distDir, { recursive: true, force: true })
---
+++ dist/Logo/GitLab.js
@@ -0,0 +1,39 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var GitLab = function GitLab(_ref) {
+  var _ref$fill = _ref.fill,
+    fill = _ref$fill === void 0 ? {} : _ref$fill,
+    _ref$w = _ref.w,
+    w = _ref$w === void 0 ? "45" : _ref$w,
+    _ref$h = _ref.h,
+    h = _ref$h === void 0 ? "45" : _ref$h;
+  return /*#__PURE__*/_react["default"].createElement("svg", {
+    style: fill,
+    role: "img",
+    viewBox: "0 0 24 24",
+    xmlns: "http://www.w3.org/2000/svg",
+    width: w,
+    height: h
+  }, /*#__PURE__*/_react["default"].createElement("title", null, "GitLab"), /*#__PURE__*/_react["default"].createElement("polygon", {
+    points: "16 28.896 16 28.896 21.156 13.029 10.844 13.029 16 28.896"
+  }), /*#__PURE__*/_react["default"].createElement("polygon", {
+    points: "16 28.896 10.844 13.029 3.619 13.029 16 28.896"
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M3.619,13.029h0L2.052,17.851a1.067,1.067,0,0,0,.388,1.193L16,28.9,3.619,13.029Z"
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M3.619,13.029h7.225L7.739,3.473a.534.534,0,0,0-1.015,0L3.619,13.029Z"
+  }), /*#__PURE__*/_react["default"].createElement("polygon", {
+    points: "16 28.896 21.156 13.029 28.381 13.029 16 28.896"
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M28.381,13.029h0l1.567,4.822a1.067,1.067,0,0,1-.388,1.193L16,28.9,28.381,13.029Z"
+  }), /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M28.381,13.029H21.156l3.105-9.557a.534.534,0,0,1,1.015,0l3.105,9.557Z"
+  }));
+};
+var _default = exports["default"] = GitLab;
